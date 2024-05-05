8dba92463e6b2c2d9bb7874be7d8ac091e7aaa16
NikitaEfimov0 <nikitaef2002@gmail.com>
2024-05-05 18:37:38 +0300

    fixed webpack for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,69 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-module.exports = {
-  mode: "development",
-  entry: ["./src/index.tsx"],
-  devtool: "inline-source-map",
-  module: {
-    rules: [
+
+var config = {
+  mode : "development" ,
+  entry : ["./src/index.tsx"] ,
+  devtool : "inline-source-map" ,
+  module : {
+    rules : [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+        test : /\.css$/i ,
+        use : ["style-loader" , "css-loader"] ,
+      } ,
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset",
-      },
+        test : /\.(png|jpe?g|gif|svg)$/i ,
+        type : "asset" ,
+      } ,
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        type: "asset/resource",
-      },
+        test : /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/ ,
+        type : "asset/resource" ,
+      } ,
       {
-        test: /\.(?:js|mjs|cjs|tsx|ts|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader",
+        test : /\.(?:js|mjs|cjs|tsx|ts|jsx)$/ ,
+        exclude : /node_modules/ ,
+        use : {
+          loader : "ts-loader" ,
           // options: {
           // 	presets: [["@babel/preset-env", { targets: "defaults" }]],
           // },
-        },
-      },
-    ],
-  },
-  watch: true,
-  resolve: {
-    extensions: [".*", ".js", ".jsx", ".ts", ".tsx"],
-    alias: {
-      "@src": path.resolve(__dirname, "src"),
-    },
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
-  },
+        } ,
+      } ,
+    ] ,
+  } ,
+  watch : true ,
+  resolve : {
+    extensions : [".*" , ".js" , ".jsx" , ".ts" , ".tsx"] ,
+    alias : {
+      "@src" : path.resolve(__dirname , "src") ,
+    } ,
+  } ,
+  output : {
+    path : path.resolve(__dirname , "dist") ,
+    filename : "bundle.js" ,
+    publicPath : "/" ,
+  } ,
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+  plugins : [
+    new webpack.HotModuleReplacementPlugin() ,
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
-      title: "Webpack App",
-      favicon: "./public/logo_short.svg",
-    }),
-  ],
+      template : "./public/index.html" ,
+      filename : "index.html" ,
+      title : "Webpack App" ,
+      favicon : "./public/logo_short.svg" ,
+    }) ,
+  ] ,
 
-  devServer: {
-    static: path.resolve(__dirname, "dist"),
-    hot: true,
-    historyApiFallback: true,
-    port: 3002,
-  },
+  devServer : {
+    static : path.resolve(__dirname , "dist") ,
+    hot : true ,
+    historyApiFallback : true ,
+    port : 3002 ,
+  } ,
+}
+module.exports =(env, argv) => {
+  config.mode=argv.mode || "development"
+  config.watch=argv.mode!=="production"
+  return config
 };
