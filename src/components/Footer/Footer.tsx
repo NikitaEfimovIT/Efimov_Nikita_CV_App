929b2432cbace1c9e2e33e56abc02d9ee4088709
NikitaEfimov0 <nikitaef2002@gmail.com>
2025-02-06 18:57:38 +0300

    fixed date

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     <Container
       style={{ width: "100%", textAlign: "center", display: "flex", justifyContent: "center", alignItems: "center" }}
     >
-      <h3 style={{ color: "white", fontWeight: 400, fontSize: "1em" }}>©️2024 Efimov Nikita</h3>
+      <h3 style={{ color: "white", fontWeight: 400, fontSize: "1em" }}>©️2025 Efimov Nikita</h3>
     </Container>
   );
 };
